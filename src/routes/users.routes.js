95a4377d26e0bc2675b549cd46ff4a667199e6d2
unknown <fabioflauzino@hotmail.com>
2024-06-29 17:52:57 -0300

    Salvando arquivos

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,12 +2,18 @@ const { Router } = require("express")
 
 const UserController = require("../controllers/UserController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
+const UserAvatarController = require("../controllers/UserAvatarController")
 
 const userRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const userController = new UserController()
+const userAvatarController = new UserAvatarController()
 
 userRoutes.post("/", userController.create)
 userRoutes.put("/", ensureAuthenticated,userController.update)
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = userRoutes
